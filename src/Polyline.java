b3a980d79006daeca54b400990a15b8deda67474
Cactooz <54435874+Cactooz@users.noreply.github.com>
2021-12-06 11:26:26 +0100

    Base constructors

--- src/Polyline.java
+++ src/Polyline.java
@@ -2,4 +2,15 @@ public class Polyline {
 	private Point[] points;
 	private String color = "black";
 	private int width = 1;
+	
+	public Polyline() {
+		points = new Point[0];
+	}
+	
+	public Polyline(Point[] points) {
+		points = new Point[points.length];
+		for(int i = 0; i < points.length; i++) {
+			points[i] = new Point(points[i]);
+		}
+	}
 }
