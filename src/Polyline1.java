5448382536403a9dd591532a87c09a5ec3985688
Cactooz <54435874+Cactooz@users.noreply.github.com>
2021-12-08 09:44:12 +0100

    Polyline1 class

---
+++ src/Polyline1.java
@@ -0,0 +1,37 @@
+public class Polyline1 {
+	private Point[] points;
+	private String color = "black";
+	private int width = 1;
+	
+	public Polyline1() {
+		points = new Point[0];
+	}
+	
+	public Polyline1(Point[] points) {
+		points = new Point[points.length];
+		for(int i = 0; i < points.length; i++) {
+			points[i] = new Point(points[i]);
+		}
+	}
+	
+	public Point[] getPoints() {
+		return points;
+	}
+	
+	public String getColor() {
+		return color;
+	}
+	
+	public int getWidth() {
+		return width;
+	}
+	
+	public void setColor(String newColor) {
+		color = newColor;
+	}
+	
+	public void setWidth(int newWidth) {
+		width = newWidth;
+	}
+	
+}
