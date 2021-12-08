c58303a39afbd47f5549a3af9263586c9b24741b
Cactooz <54435874+Cactooz@users.noreply.github.com>
2021-12-08 10:59:36 +0100

    Fixed addPointStart out of bounds loop issue

--- src/Polyline.java
+++ src/Polyline.java
@@ -78,7 +78,7 @@ public class Polyline {
 		tempPoints[0] = newPoint;
 		
 		//Add the old points to the array
-		for(int i = 1; i < tempPoints.length; i++) {
+		for(int i = 1; i < tempPoints.length-1; i++) {
 			tempPoints[i] = points[i];
 		}
 		
