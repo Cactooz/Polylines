11add8daee934b21099f9de44bddbd8df0f6294e
Cactooz <54435874+Cactooz@users.noreply.github.com>
2021-12-13 10:59:37 +0100

    Simple program for getting and setting Polyline variables

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-import java.io.PrintWriter;
+import java.util.Arrays;
 
 public class Main {
 	public static void main(String[] args) {
@@ -27,5 +27,21 @@ public class Main {
 		//Add a point p with the same attributes as p1
 		Point p = new Point(p1);
 		System.out.println(p);
+		
+		Point[] polylinePointer = new Point[]{new Point("A", 5, 1), new Point("B", 3, 4), new Point("C", 6, 1), new Point("D", 3, 2)};
+		
+		Polyline poly1 = new Polyline(polylinePointer);
+		System.out.println("Width: " + poly1.getWidth());
+		System.out.println("Color: " + poly1.getColor());
+		poly1.setColor("blue");
+		poly1.setWidth(2);
+		System.out.println("Length: " + poly1.length());
+		System.out.println(Arrays.toString(poly1.getPoints()));
+		
+		Point p10 = new Point("Q", 10, 2);
+		poly1.addPointStart(p10);
+		System.out.println(poly1.toString());
+		poly1.remove("D");
+		System.out.println(poly1.toString());
 	}
 }
