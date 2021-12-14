6a24b5a136f6cbdea295c6a89b15e3213603581c
Cactooz <54435874+Cactooz@users.noreply.github.com>
2021-12-14 11:28:37 +0100

    Method to create a random point

--- src/ChoosePolyline.java
+++ src/ChoosePolyline.java
@@ -1,5 +1,21 @@
+import java.util.Random;
+
 public class ChoosePolyline {
+	static final Random random = new Random();
+	
 	public static void main(String[] args) {
 	
+	}
+	
+	//Create a random point
+	private static Point randomPoint() {
+		//Give the point a random letter
+		String name = "" + (char) (65 + random.nextInt(26));
+		
+		//Give the point random coordinates
+		int x = random.nextInt(20);
+		int y = random.nextInt(20);
+		
+		return new Point(name, x, y);
 	}
 }
